e985f86d117feafb9efdf1580a051966231e81b0
Sanjeet Prajapati <spayodhya224172@gmail.com>
2025-07-14 07:02:14 +0530

    dbms-project

---
+++ sql1.sql
@@ -0,0 +1,7 @@
+create database demo;
+use demo;
+create table study(id int(10),name varchar(30),email varchar(30), mobile varchar(10));
+show tables;
+insert into study(id,name,email,mobile) values(1,'ram','ram@gmail.com','5465765654');
+select*from study;
+
